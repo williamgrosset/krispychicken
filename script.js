1796a0363e9ac4545494ae97b44cc8829c8c7bb7
Louis Ritchie <louiscritchie@gmail.com>
2016-08-27 13:24:24 -0700

    spawn chicken in center of page

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
 var imgURL = chrome.extension.getURL('chicken.png');
 
-document.body.style.backgroundImage='url(' + imgURL + ')';
+var div = document.createElement('div');
+div.style.height="157px";
+div.style.width="321px";
+div.style.position="absolute";
+div.style.bottom="50%";
+div.style.left="40%";
+div.style.zIndex="5";
+div.style.backgroundImage='url(' + imgURL + ')';
+document.body.appendChild(div);
