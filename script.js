9c331e13a4ac2102dfaa7fe00c7da508a7a28cc8
William <will_grosset@hotmail.com>
2016-08-27 14:13:47 -0700

    added chicken for every img on web page

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 var imgURL = chrome.extension.getURL('chicken.png');
 
-var div = document.createElement('div');
+/*var div = document.createElement('div');
 div.style.height="157px";
 div.style.width="321px";
 div.style.position="absolute";
@@ -8,9 +8,27 @@ div.style.bottom="50%";
 div.style.left="40%";
 div.style.zIndex="5";
 div.style.backgroundImage='url(' + imgURL + ')';
-document.body.appendChild(div);
+document.body.appendChild(div);*/
 
 var imgURL = chrome.extension.getURL('chicken.png'); 
-document.body.style.backgroundImage='url(' + imgURL + ')'; 
+//document.body.style.backgroundImage='url(' + imgURL + ')'; 
+
+function replaceImages(domImg, srcImage) {
+    var img = new Image();
+    img.onload = function()
+    {
+        // Load completed
+        domImg.src = this.src;
+    };
+    img.src = srcImage;
+}
+
+var imgs = document.getElementsByTagName('img');
+for (var i  = 0; i < imgs.length; i++) {
+    imgs[i].src = replaceImages(imgs[i], imgURL);
+}
+
+
+// FIX ME (AUDIO)
 var audio = new Audio('duffman.mp3'); 
 audio.play();
