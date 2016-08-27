90e05e10a79dc53165841a1c8d3da9bee8e5305a
William <will_grosset@hotmail.com>
2016-08-27 13:00:32 -0700

    fixed chicken images

--- popup.js
+++ popup.js
@@ -1,11 +1,13 @@
 // Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
-
+var chickens = "chicken.png";
 
 function click(e) {
+  console.log("We are in popup.js function");
+  //document.body.style.backgroundImage = "url('chicken.png')";
   chrome.tabs.executeScript(null,
-      {code:"document.body.style.backgroundColor='" + e.target.id + "'"});
+      {code:"var imgURL = chrome.extension.getURL('chicken.png'); document.body.style.backgroundImage='url(' + imgURL + ')'"});
   window.close();
 }
 
