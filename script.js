754115b46aa5c0ee57888fd5d09b40419d5971d4
Louis Ritchie <louiscritchie@gmail.com>
2016-08-27 14:34:15 -0700

    shift the page on random intervals

--- script.js
+++ script.js
@@ -12,5 +12,21 @@ document.body.appendChild(div);
 
 var imgURL = chrome.extension.getURL('chicken.png'); 
 document.body.style.backgroundImage='url(' + imgURL + ')'; 
+
 var audio = new Audio('duffman.mp3'); 
 audio.play();
+
+var divs = document.querySelectorAll('div');
+for (var i = 0; i < divs.length; i++) {
+  divs[i].style.transition='all 0.3s';
+  twitchSometimes(divs[i]);
+}
+
+function twitchSometimes(div) {
+  var randomInterval = Math.random() * 10000;
+  setInterval(function() {changeRotation(div);}, randomInterval);
+}
+
+function changeRotation(div) {
+  div.style.transform='rotate(0.5deg)';
+}
