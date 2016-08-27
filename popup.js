c015220050c47218826e527d49fba93e7a411728
Louis Ritchie <louiscritchie@gmail.com>
2016-08-27 15:55:02 -0700

    seven groups of elements that move together

--- popup.js
+++ popup.js
@@ -1,6 +1,8 @@
 // Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
+var chickens = "chicken.png";
+
 function click(e) {
   chrome.tabs.executeScript(null,
       {file:"script.js"}
